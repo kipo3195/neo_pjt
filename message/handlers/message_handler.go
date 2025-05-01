061ecf095110837c95ac5d395a9ddc42ab5c0d3b
kipo3195 <bse3808@gmail.com>
2025-05-01 22:46:03 +0900

    채널 개념 도입 - 4 (클린아키텍처에서 pub.sub처리 진행중)

--- message/handlers/message_handler.go
+++ message/handlers/message_handler.go
@@ -8,11 +8,15 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/nats-io/nats.go"
 )
 
 const (
-	TYPE = "type"
+	TYPE = "type" // 이벤트의 대분류
+	CMD  = "cmd"  // 이벤트 type의 세부 타입
 	AUTH = "auth"
+	CHAT = "chat"
+	NOTE = "note"
 )
 
 var upgrader = websocket.Upgrader{
@@ -36,6 +40,41 @@ func NewMessageHandler(cu usecases.ChatUsecase, au usecases.AuthUsecase, mb inte
 	}
 }
 
+// 구독 정보 interface Unsubscribe를 가짐
+// nats의 NatsSubscription은 Unsubscribe를 가지고 있기 때문에 처리가능함.
+type Subscription interface {
+	Unsubscribe() error
+}
+
+/*NATS용 래퍼 정의*/
+type NatsSubscription struct {
+	sub *nats.Subscription
+}
+
+/*NATS용 구독해제 메소드 정의 */
+func (n *NatsSubscription) Unsubscribe() error {
+	return n.sub.Unsubscribe()
+}
+
+/*NATS용 브로커 (덕타이핑)*/
+type NatsBroker struct {
+	conn *nats.Conn
+}
+
+/*NATS용 구독 메소드 정의 */
+func (b *NatsBroker) Subscribe(roomId string) (Subscription, error) {
+	msgChan := make(chan *nats.Msg, 64)
+	natsSub, err := b.conn.ChanSubscribe(roomId, msgChan)
+	if err != nil {
+		return nil, err
+	}
+	return &NatsSubscription{sub: natsSub}, nil
+}
+
+type incomming struct {
+	Content string `json:"content"`
+}
+
 // MessageHandler의 메소드 SetupRoutes에서 등록됨.
 func (h *MessageHandler) HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 
@@ -49,6 +88,8 @@ func (h *MessageHandler) HandleWebSocket(w http.ResponseWriter, r *http.Request)
 	// 인증 처리 flag
 	authenticated := false
 
+	var sub Subscription // 구독 정보
+
 	for {
 		// 메시지는 반복해서 수신
 		_, msg, err := conn.ReadMessage()
@@ -57,15 +98,15 @@ func (h *MessageHandler) HandleWebSocket(w http.ResponseWriter, r *http.Request)
 		}
 
 		/* 페이로드 검증 */
-		var payload map[string]interface{}
-		json.Unmarshal(msg, &payload)
-		fmt.Println("웹소켓 메시지 수신시 최초 로깅 : ", payload)
-		msgType := payload[TYPE].(string)
+		var data map[string]interface{}
+		json.Unmarshal(msg, &data)
+		fmt.Println("웹소켓 메시지 수신시 최초 로깅 : ", data)
+		msgType := data[TYPE].(string)
 
-		// 최초 인증 처리
+		// 최초 인증 처리 -> 모듈화? 방안 모색 TODO
 		if !authenticated {
 			if msgType == AUTH {
-				token := payload["token"].(string)
+				token := data["token"].(string)
 
 				flag, err := h.au.AuthenticateToken(token)
 				if err != nil {
@@ -75,19 +116,60 @@ func (h *MessageHandler) HandleWebSocket(w http.ResponseWriter, r *http.Request)
 				}
 				authenticated = flag
 				conn.WriteMessage(websocket.TextMessage, []byte(`{"success":"auth_ok"}`))
+				// continue하지않으면 아래로 내려가 버리기 때문에 인증완료 후에는 다시 메시지 수신(블로킹)
+				continue
 			} else {
 				// 인증 안 된 상태에서는 다른 메시지 못 보내게 함
 				// defer conn.Close() 호출 되므로 연결 종료.
 				conn.WriteMessage(websocket.TextMessage, []byte(`{"error":"unauthorized"}`))
 				break
 			}
+
 		}
 
-		fmt.Println("인증 완료된 계정 :", payload["user_id"])
+		fmt.Println("인증 완료된 계정 :", data["userId"])
 
-		// 여기서 부터 케이스에 따른 처리
-		// 채팅방 구독, 채팅방 구독 취소
-		// 쪽지
+		// 채팅방을 구독하더라도 쪽지를 보낼 수 있어야 한다.
+		cmd := data[CMD].(string)
+		payload := data["payload"].(map[string]interface{})
+		if msgType == CHAT {
+
+			msgChan := make(chan *nats.Msg, 64) // 채널은 생성, 채널에 데이터를 구독하는건 joinRoom에서
+
+			roomId := payload["roomId"].(string)
+			if cmd == "joinRoom" {
+				// 채널 생성
+
+				// interface였던 브로커를 꺼내기 위함. -> 다른 mb라면???
+				natsSub, err := h.mb.(*nats.Conn).ChanSubscribe(roomId, msgChan)
+				if err != nil {
+					log.Println("NATS subscribe error:", err)
+					return
+				}
+				// 래퍼로 감싸서 인터페이스로 저장
+				sub = &NatsSubscription{sub: natsSub}
+				fmt.Printf("roomId : %s 구독 하였습니다.\n", roomId)
+
+			} else if cmd == "joinRoomCancel" {
+				if sub != nil {
+					sub.Unsubscribe() // 인터페이스 메서드 호출 (OK!)
+					sub = nil
+					fmt.Printf("roomId : %s 구독해제 하였습니다.\n", roomId)
+				}
+			} else if cmd == "sendMessage" {
+				// 별도의 고루틴으로 처리?
+				content := payload["content"].([]byte)
+				if err := h.mb.(*nats.Conn).Publish(roomId, content); err != nil {
+					log.Println("NATS publish error:", err)
+				}
+			}
+
+		} else if msgType == NOTE {
+			if cmd == "sendMessage" {
+
+			}
+
+		}
 
 	}
 
