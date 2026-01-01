196e6e0ecea13a5553f2ad6d65b2b375d4b34d8d
kipo3195 <bse3808@gmail.com>
2026-01-01 11:24:35 +0900

    모든 서비스의 service_users 모델 최신화

--- organization/internal/infrastructure/model/service_users.go
+++ organization/internal/infrastructure/model/service_users.go
@@ -3,9 +3,10 @@ package model
 import "time"
 
 type ServiceUsers struct {
-	UserHash string    `gorm:"column:user_hash;primaryKey;comment:pk"`
-	UserId   string    `gorm:"column:user_id;comment:사용자 ID"`
-	UseYn    string    `gorm:"column:use_yn;default:Y;comment:사용여부"`
+	Org      string    `gorm:"column:org;varchar(30);primaryKey;comment:works code"`
+	UserId   string    `gorm:"column:user_id;varchar(191);primaryKey;comment:사용자 ID"`
+	UserHash string    `gorm:"column:user_hash;varchar(191);comment:사용자 hash 정보"`
+	UseYn    string    `gorm:"column:use_yn;varchar(3);default:Y;comment:사용여부"`
 	CreateAt time.Time `gorm:"column:create_at;autoCreateTime;comment:DB 저장시간"`
 }
 
@@ -13,4 +14,7 @@ func (ServiceUsers) TableName() string {
 	return "service_users"
 }
 
-// 서비스 등록 사용자 (공통)
+// 서비스 등록 사용자 (공통) 20260101 기준으로 작성.
+// user_id는 변할 수 있지만, user_hash는 변할 수 없으므로 pk는 org와 userId로 처리한다.
+// 단, 필요에 의해 org, user_hash를 pk로 두고 `연동 계정` 이라는 별도의 테이블을 생성해서 user_hash로 매핑 하는 방안도 사용할 수 있다.
+// id, hash의 길이가 191인 것은 gorm의 autoMigrate에 의해 테이블이 생성될때 string에 매핑되는 고유 길이다.
