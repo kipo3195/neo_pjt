45a62374d431830501bfd3e149de3c00111f70a7
kipo3195 <bse3808@gmail.com>
2026-02-11 00:10:00 +0900

    테스트 로직 제거

--- message/internal/adapter/http/handler/line_key_handler.go
+++ message/internal/adapter/http/handler/line_key_handler.go
@@ -1,11 +1,9 @@
 package handler
 
 import (
-	"log"
 	"message/internal/adapter/http/dto/lineKey"
 	"message/internal/application/usecase"
 	response "message/pkg/response"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,10 +21,8 @@ func NewLineKeyHandler(usecase usecase.LineKeyUsecase) *LineKeyHandler {
 func (h *LineKeyHandler) GetLineKey(c *gin.Context) {
 
 	ctx := c.Request.Context()
-	log.Println("라인키 발급 전 sleep")
-	time.Sleep(15 * time.Second)
+
 	temp, _ := h.usecase.GetLineKey(ctx)
-	log.Println("라인키 발급 temp")
 
 	res := lineKey.GetLineKeyResponse{
 		LineKey: temp,
